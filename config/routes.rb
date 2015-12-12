2e10e4f468e8b3da8897d9b4839a3bf68034503e
ajhausdorf <ajhausdorf@wustl.edu>
2015-12-12 01:07:02 -0600

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/trips'
+
+  get 'static_pages/hosts'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/blog'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
